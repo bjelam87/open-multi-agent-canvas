3d0ee56812f07d2a03b1e8d86e525f2a1d4540a5
Nathan Tarbert <nathantarbert@Mac.lan>
2025-02-13 17:06:18 -0500

    remove nango

--- frontend/src/lib/nango-backend.ts
+++
@@ -1,7 +0,0 @@
-import { Nango } from "@nangohq/node";
-
-const nangoBackend = new Nango({
-  secretKey: process.env.NANGO_SECRET_KEY!,
-});
-
-export default nangoBackend;
--- frontend/src/lib/nango-frontend.ts
+++
@@ -1,7 +0,0 @@
-import Nango from "@nangohq/frontend";
-
-const nangoFrontend = new Nango({
-  publicKey: process.env.NEXT_PUBLIC_NANGO_PUBLIC_KEY as string,
-});
-
-export default nangoFrontend;
